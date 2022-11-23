52a18e517efc0233d81e3d3f16f0f79ab691f5fe
irina mihai <irinamihai150@gmail.com>
2022-11-23 23:27:53 +0000

    style

--- src/Card.js
+++ src/Card.js
@@ -3,38 +3,56 @@ import jobListing from "./List";
 
 export default function Card() {
   return (
-    <div className="container">
+    <div className="card-container">
       {jobListing.map((job) => {
         return (
-          <div className="test">
-            <div className="main-part">
-              <div className="company-container">
-                <p key={job.id}> {job.company}</p>
-                <p key={job.id}> {job.new}</p>
-                <p key={job.id}>{job.new ? <p>New</p> : ""}</p>
-                <p key={job.id}>{job.featured ? <p>Featured</p> : ""}</p>
-              </div>
-              <div className="article2">
-                <div className="location-container">
-                  <div className="title-container">
-                    <h3 key={job.id}>{job.position}</h3>
-                  </div>
-
+          <div className="card">
+            <div className="article">
+              {/* <img  src = "./img" alt ="logo"/> */}
+              <div className="job-list-details">
+                <div className="company-details">
+                  <p className="company-name" key={job.id}>
+                    {" "}
+                    {job.company}
+                  </p>
+                  <p key={job.id}> {job.new}</p>
+                  <p key={job.id} className="new">
+                    {job.new ? <p>New</p> : ""}
+                  </p>
+                  <p key={job.id} className="featured">
+                    {job.featured ? <p>Featured</p> : ""}
+                  </p>
+                </div>
+                <div className="sub-article">
+                  <h3 key={job.id} className="position-container">
+                    {job.position}
+                  </h3>
                   <div id="contain-loc">
-                    <p key={job.id}>{job.postedAt}</p>
-                    <p key={job.id}>{job.contract}</p>
-                    <p key={job.id}>{job.location}</p>
+                    <p key={job.id} className="posted-container">
+                      {job.postedAt}
+                    </p>
+
+                    <p key={job.id} className="contract-container">
+                      {job.contract}
+                    </p>
+                    <p key={job.id} className="location-container">
+                      {job.location}
+                    </p>
                   </div>
                 </div>
               </div>
             </div>
             <div className="roles-container">
-              <p key={job.id}>{job.role}</p>
-              <p key={job.id}>{job.level}</p>
+              <p key={job.id} className="role-container">
+                {job.role}
+              </p>
+              <p key={job.id} className="level-container">
+                {job.level}
+              </p>
               <p key={job.id} className="languages-container">
                 <ul>
                   {job.languages.map((language) => (
-                    <li style={{ color: "blue" }}>{language}</li>
+                    <li>{language}</li>
                   ))}
                 </ul>
               </p>
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from "react";
+import header from "./images/header.svg";
 
 export default function Header() {
   return (
     <div>
       <header>
-      <img src="https://images.unsplash.com/photo-1494500764479-0c8f2919a3d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt ="logo"/>
+        <img src={header} alt="blue" />
       </header>
     </div>
   );
